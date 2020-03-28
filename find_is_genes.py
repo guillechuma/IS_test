ad91f1dc772dd6ac6164c1a8ccfff84a777cc7ff
Guillermo <guillechuma24@gmail.com>
2020-03-28 02:25:24 +0100

    overlap file is complete

--- find_is_genes.py
+++ find_is_genes.py
@@ -7,6 +7,7 @@ from parse_isescan import parse_isescan
 from parse_genome import parse_genome
 import numpy as np
 import pandas as pd
+import pyranges as pr
 
 def create_csv_files(genome_name):
 	'''
@@ -29,7 +30,17 @@ def find_is_genes(genome_name):
 	is_df = is_df[is_df['type'] == 'insertion_sequence']
 	
 	
+	is_range = pr.PyRanges(chromosomes=genome_name,starts=is_df['start'], ends=is_df['end'],strands=is_df['strand'])
 	
+	genome_range = pr.PyRanges(chromosomes=genome_name,starts=genome_df['start'], ends=genome_df['end'],strands=genome_df['strand'])
+	genome_range.id = genome_df['id']
+	
+	intersected_df = genome_range.overlap(is_range).as_df()
+	
+	merged_df = pd.merge(genome_df, intersected_df, on='id')
+	print('There are ' + str(len(merged_df)) + ' IS sequences interrupting genes in ' + genome_name)
+	print('The genes are:')
+	print(merged_df['gene_name'])
 	
 	
 	
